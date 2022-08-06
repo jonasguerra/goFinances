aa3a48e55260b37a6179b2226d14eadbd26f5a05
Jonas Guerra <guerra.jonas@hotmail.com>
2022-08-06 16:15:14 -0300

    Adicionado arquivo de configuração para o Jest

--- src/__tests__/screens/profile.test.tsx
+++ src/__tests__/screens/profile.test.tsx
@@ -2,10 +2,31 @@ import React from "react";
 import { render } from "@testing-library/react-native";
 import { Profile } from "../../screens/Profile";
 
-test("Check if user placeholder name is shown correctly", () => {
-  const { getByPlaceholderText } = render(<Profile />);
+describe("Profile tests", () => {
+  it("Check if user placeholder name is shown correctly", () => {
+    //capturar com o placeholder
+    const { getByPlaceholderText } = render(<Profile />);
 
-  const inputName = getByPlaceholderText("Nome");
+    const inputName = getByPlaceholderText("Nome");
 
-  expect(inputName.props.placeholder).toBeTruthy();
+    expect(inputName).toBeTruthy();
+  });
+
+  it("Check if user data has been loaded", () => {
+    //utilizando o testID no componente
+    const { getByTestId } = render(<Profile />);
+
+    const inputName = getByTestId("input-name");
+    const inputSurname = getByTestId("input-surname");
+
+    expect(inputName.props.value).toEqual("Jonas");
+    expect(inputSurname.props.value).toEqual("Guerra");
+  });
+
+  it("Checks if title render correctly", () => {
+    const { debug, getByTestId } = render(<Profile />);
+
+    const textTitle = getByTestId("text-title");
+    expect(textTitle.props.children).toContain("Profile");
+  });
 });
--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -5,9 +5,18 @@ import { Button, Text, TextInput, View } from "react-native";
 export function Profile() {
   return (
     <View>
-      <Text>Profile</Text>
-      <TextInput placeholder="Nome" autoCorrect={false} />
-      <TextInput placeholder="Sobrenome" />
+      <Text testID="text-title">Profile</Text>
+      <TextInput
+        testID="input-name"
+        placeholder="Nome"
+        autoCorrect={false}
+        value="Jonas"
+      />
+      <TextInput
+        testID="input-surname"
+        placeholder="Sobrenome"
+        value="Guerra"
+      />
       <Button title="Salvar" onPress={() => {}} />
     </View>
   );
